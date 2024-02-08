0a2bd08a74dca33841afc4f8e71b9892f433a077
waltersimekha <waltersimekha@gmail.com>
2024-02-09 01:14:33 +0300

    completed assignment

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Article() {
+function AArticle() {
   return (
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
 
-export default Article;
+export default AArticle;
